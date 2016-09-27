97e79b4d76fa3f252d4250052ebc127678d9109f
Marshall Wu <hao.wu@citrix.com>
2016-09-27 18:00:19 +0800

    Signed-off-by: Marshall Wu <hao.wu@citrix.com>

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2016, Read the Docs'
+project = u'Citrix SDK'
+copyright = u'2016, Citrix Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
