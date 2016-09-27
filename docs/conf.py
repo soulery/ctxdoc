b8f669371ec96cae6e5320167b577fbc3ac2ba23
Marshall Wu <hao.wu@citrix.com>
2016-09-27 18:14:49 +0800

    Signed-off-by: Marshall Wu <hao.wu@citrix.com>

--- docs/conf.py
+++ docs/conf.py
@@ -98,14 +98,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {    'github_button': False,
-    'github_banner': True,
-    'show_powered_by': False,}
+#html_theme_options = {    'github_button': False,'github_banner': False}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
